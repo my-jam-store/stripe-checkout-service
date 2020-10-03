20c15dcee2260c5d8fcb91de0e3d85247a605c86
Akai <akai-z@users.noreply.github.com>
2020-10-03 22:39:25 +0300

    Remove an unnecessary newline in create-session route

--- src/services/routes/create-session.js
+++ src/services/routes/create-session.js
@@ -6,7 +6,6 @@ function setRoute(app, express) {
   app.post(`/${routeName}`, express.json(), routeHandler)
 }
 
-
 async function routeHandler(req, res) {
   try {
     const session = await stripe.createCheckoutSession(req.body)
