3937ea4e3d78f6a1bd013d29411a1a81b9c9d8eb
Akai <akai-z@users.noreply.github.com>
2020-11-07 21:47:07 +0200

    Add shipping product type check function to shipping module

--- src/services/checkout/shipping.js
+++ src/services/checkout/shipping.js
@@ -2,10 +2,15 @@ function amount(subtotal) {
   return !isFreeShipping(subtotal) ? process.env.SHIPPING_FEE * 100 : 0
 }
 
+function isShippingProduct(productType) {
+  return productType === 'shipping_fee'
+}
+
 function isFreeShipping(subtotal) {
   return subtotal >= process.env.FREE_SHIPPING_SUBTOTAL
 }
 
 module.exports = {
-  amount
+  amount,
+  isShippingProduct
 }
